023b550b04f65c85eb836d3561daa231c37bd380
Rohit <raghunagarohitkampati@gmail.com>
2026-07-03 14:32:35 +0530

    Remove debug console logs, clean production build

--- frontend/src/pages/dashboards/LeadDashboard.tsx
+++ frontend/src/pages/dashboards/LeadDashboard.tsx
@@ -48,12 +48,6 @@ export const LeadDashboard: React.FC = () => {
     return inTeam || reportsToMe;
   });
 
-  // DEBUG: Remove after fix confirmed
-  console.log('[LeadDashboard] currentUser email:', currentUser?.email);
-  console.log('[LeadDashboard] myTeamIds:', myTeamIds);
-  console.log('[LeadDashboard] total users:', users.length);
-  console.log('[LeadDashboard] teamMembers found:', teamMembers.length, teamMembers.map(m => m.full_name));
-  console.log('[LeadDashboard] sample user reporting_to_list:', users[0]?.reporting_to_list);
 
 
   // Applications assigned to team members
